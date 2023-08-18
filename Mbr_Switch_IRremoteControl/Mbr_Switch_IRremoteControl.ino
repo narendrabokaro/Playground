f04848d94149db701094b2474a63122da6af07f6
Narendra Singh <narendra.bokaro@gmail.com>
2023-08-18 19:36:52 +0530

    documentation + code optimization + reduce memory write cycle

--- Mbr_Switch_IRremoteControl/Mbr_Switch_IRremoteControl.ino
+++ Mbr_Switch_IRremoteControl/Mbr_Switch_IRremoteControl.ino
@@ -5,9 +5,11 @@
     > Maintain the device state inside ESP 8266 EEPROM memory.
   version - 2.0.0
  *************************************************************/
+
 #include <IRremote.h>
 #include <EEPROM.h>
 
+// Reciver GPIO pin
 IRrecv IR(D7);
 
 #define fanRelay D3
@@ -45,19 +47,38 @@ void setup() {
     actionBasedOnDeviceState();
 }
 
+/*
+* @function writeMemory
+* @description Method used to write on EEPROM memory only if the value is different. it will help to keep write cycle count minimum.
+* @param {int} addr Memory Address
+* @param {int} writeValue indicate the value to be written on memory location
+*/
+void writeMemory(int addr, int writeValue) {
+    // Write operation only when there is change in value
+    if (readMemory(addr) != writeValue) {
+        Serial.println("Written on memory with value : ");
+        Serial.println(writeValue);
+        // Write the memory address
+        EEPROM.write(addr, writeValue);
+        EEPROM.commit();
+    }
+}
+
+/*
+* @function readMemory
+* @description Method used to read from EEPROM memory
+* @param {int} addr Memory Address
+* @return {int} the value written on given memory location
+*/
 int readMemory(int addr) {
     // Read the memory address
     return EEPROM.read(addr);
 }
 
-void writeMemory(int addr, int writeValue) {
-    Serial.println("Written on memory with value : ");
-    Serial.println(writeValue);
-    // Write the memory address
-    EEPROM.write(addr, writeValue);
-    EEPROM.commit();
-}
-
+/*
+* @function actionBasedOnDeviceState
+* @description Method used to check if any previouly unclosed device status, if found then turn On the device
+*/
 void actionBasedOnDeviceState() {
     // Check for the fan last state
     if (readMemory(fanMemAddr) == 1) {
@@ -88,28 +109,24 @@ void loop() {
         // TODO - Read and write the approriate code for buttons
         // Switch On the FAN When IR remote button 1 is pressed
         if (IR.decodedIRData.decodedRawData == 0xF30CFF00) {
-            // digitalWrite(fanRelay, LOW);
             turnDevice(fanRelay, 1);
             Serial.println("Button 1 pressed");
         }
 
         // Switch Off the FAN When IR remote button 2 is pressed
         if (IR.decodedIRData.decodedRawData == 0xE718FF00) {
-            // digitalWrite(fanRelay, HIGH);
             turnDevice(fanRelay, 0);
             Serial.println("Button 2 pressed");
         }
 
         // Switch On the TubeLight When IR remote button 3 is pressed
         if (IR.decodedIRData.decodedRawData == 0xA15EFF00) {
-            // digitalWrite(tubeLightRelay, LOW);
             turnDevice(tubeLightRelay, 1);
             Serial.println("Button 3 pressed");
         }
 
         // Switch Off the Tubelight When IR remote button 4 is pressed
         if (IR.decodedIRData.decodedRawData == 0xF708FF00) {
-            // digitalWrite(tubeLightRelay, HIGH);
             turnDevice(tubeLightRelay, 0);
             Serial.println("Button 4 pressed");
         }
@@ -121,9 +138,8 @@ void loop() {
   if (digitalRead(fanSwitch) == LOW && !isFanOn) {
       // Turn ON the device by making relay LOW
       // Turn OFF the device by making relay HIGH
-      // digitalWrite(fanRelay, LOW);
       turnDevice(fanRelay, 1);
-      Serial.println("Fan switched pressed ON");
+      Serial.println("Fan switch pressed ON");
 
       // Set the flag true
       isFanOn = true;
@@ -131,9 +147,8 @@ void loop() {
 
   // when FAN switch OFF
   if (digitalRead(fanSwitch) == HIGH && isFanOn) {
-      // digitalWrite(fanRelay, HIGH);
       turnDevice(fanRelay, 0);
-      Serial.println("Fan switched pressed OFF");
+      Serial.println("Fan switch pressed OFF");
 
       // Set the flag false
       isFanOn = false;
@@ -142,18 +157,16 @@ void loop() {
 
   // when TubeLight switch ON
   if (digitalRead(tubeLightSwitch) == LOW && !isTubelightOn) {
-      // digitalWrite(tubeLightRelay, LOW);
       turnDevice(tubeLightRelay, 1);
-      Serial.println("Tubelight switched pressed ON");
+      Serial.println("Tubelight switch pressed ON");
       // Set the flag true
       isTubelightOn = true;
   }
 
   // when TubeLight switch OFF
   if (digitalRead(tubeLightSwitch) == HIGH && isTubelightOn) {
-      // digitalWrite(tubeLightRelay, HIGH);
       turnDevice(tubeLightRelay, 0);
-      Serial.println("Tubelight switched pressed OFF");
+      Serial.println("Tubelight switch pressed OFF");
       // Set the flag false
       isTubelightOn = false;
       delay(100);
